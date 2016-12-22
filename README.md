c2c8464b7b8898e6ef131100a19c2e2e748f6c16
Zarino Zappia <mail@zarino.co.uk>
2016-12-22 15:38:27 +0000

    "Market" app for tracking ebay listings

--- README.md
+++ README.md
@@ -8,3 +8,24 @@ A django project, for monitoring lego prices on ebay.
     (env)$ pip install -r requirements.txt
 
 Create a file at `brickwatch/local_settings.py` to override the default settings for your specific environment.
+
+
+## Apps
+
+**market** – everything to do with ebay listings, active or ended.
+
+**catalog** – everything to do with lego sets, like their names, RRPs, and production dates.
+
+<!--
+**Model: catalog.Set**
+- set_number (string, eg: 76040)
+- set_number_variant (integer, eg: 1, from 76040-1)
+- name (string)
+- year (integer)
+- availability (string)
+- brickset_url (string)
+- image_url (string)
+- rrp_gbp (string, or DecimalField?)
+- rrp_usd (string, or DecimalField?)
+- rrp_eur (string, or DecimalField?)
+-->
---
+++ market/models.py
@@ -0,0 +1,45 @@
+from __future__ import unicode_literals
+
+from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+
+@python_2_unicode_compatible
+class Listing(models.Model):
+    SALE_TYPE_AUCTION = 0
+    SALE_TYPE_BUYITNOW = 1
+    SALE_TYPE_CHOICES = (
+        (SALE_TYPE_AUCTION, 'Auction'),
+        (SALE_TYPE_BUYITNOW, 'BuyItNow'),
+    )
+    # http://pages.ebay.com/help/sell/item-condition.html#collectibles
+    CONDITION_USED = 0
+    CONDITION_NEW = 1
+    CONDITION_CHOICES = (
+        (CONDITION_USED, 'Used'),
+        (CONDITION_NEW, 'New'),
+    )
+    listing_id = models.CharField(max_length=32, primary_key=True)
+    listing_url = models.URLField(max_length=512)
+    title = models.CharField(max_length=256)
+    sale_type = models.IntegerField(default=0, choices=SALE_TYPE_CHOICES, null=True)
+    condition = models.IntegerField(default=0, choices=CONDITION_CHOICES, null=True)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    seller_user_id = models.CharField(max_length=64)
+
+    # TODO: Each Listing can be associated with one or more Sets.
+    # catalog_items = models.ManyToManyField('catalog.Set')
+
+    bid_count = models.IntegerField(default=0, null=True)
+    start_price = models.DecimalField(max_digits=6, decimal_places=2, null=True)
+    start_price_currency = models.CharField(max_length=3, null=True)
+    current_price = models.DecimalField(max_digits=6, decimal_places=2, null=True)
+    current_price_currency = models.CharField(max_length=3, null=True)
+    shipping_cost = models.DecimalField(max_digits=6, decimal_places=2, null=True)
+    shipping_cost_currency = models.CharField(max_length=3, null=True)
+
+    buyitnow_price = models.DecimalField(max_digits=6, decimal_places=2, null=True)
+    buyitnow_price_currency = models.CharField(max_length=3, null=True)
+
+    def __str__(self):
+        return '{} {}'.format(self.listing_id, self.title)
