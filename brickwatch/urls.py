83f638cc663878b98449d741fb37bf0bb8d82757
Zarino Zappia <mail@zarino.co.uk>
2016-12-24 19:10:08 +0000

    Add a homepage!

--- brickwatch/urls.py
+++ brickwatch/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
+from brickwatch import views
+
 urlpatterns = [
     url(r'^market/', include('market.urls')),
     url(r'^catalog/', include('catalog.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^$', views.index),
 ]
